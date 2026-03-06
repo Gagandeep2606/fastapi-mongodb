3a3403c911e983ebbfd274de02ff52b2b5cc040f
Gagandeep2606 <your_github_gagankaur2686@gmail.com>
2026-03-06 14:12:45 +0530

    Added aggregation tasks: top customers and sales by city

--- orders.py
+++ orders.py
@@ -3,15 +3,17 @@ from config import orders
 
 app = FastAPI()
 
-@app.get("/")
-def get_orders():
+# Daily Sales Total
+
+@app.get("/daily-total-sales",tags=["Total Daily Sales"])
+def get_daily_total_sales(start_date:str, end_date:str):
     pipeline = [
         {
             "$match":{
                 "status":"completed",
                 "order_date":{
-                    "$gte":"2025-01-12",
-                    "$lte":"2025-01-15"
+                    "$gte":start_date,
+                    "$lte":end_date
                 }
             }
         },
@@ -44,4 +46,91 @@ def get_orders():
     result = orders.aggregate(pipeline)
     return list(result)
 
+
+# Top 5 Customers by Spend
+
+@app.get("/top-customers", tags=["Top Customers"])
+def top_customers():
     
+    pipeline = [
+         {
+             "$group":{
+                 "_id" : "$customer_id",
+                 "Total_spent":{"$sum":"$total_amount"},
+                 "Orders":{"$sum":1},
+                 "Last_order":{"$max":"$order_date"}
+             }
+         },
+         {
+             "$project":{
+                 "_id":0,
+                 "Customer":"$_id",
+                 "Total_spent":1,
+                 "Orders":1,
+                 "Last_order":1
+             }
+         },
+         {
+             "$sort": {"Total_spent": -1}
+         },
+         {
+             "$limit":5
+         }
+         
+
+    ]
+
+    result = orders.aggregate(pipeline)
+    return list(result)
+
+
+# Sales by city
+
+@app.get("/sales-by-city", tags=["Sales by City"])
+def sales_by_city():
+
+    pipeline = [
+        {
+            "$group":{
+                
+                "_id": "$city",
+                "Total_Sales" : {"$sum":"$total_amount"},
+                "Orders": {"$sum" :1},
+                "Avg_Order":{"$avg": "$total_amount"}
+            }
+        },
+        {
+            "$sort":{"Total_Sales": -1}
+        },
+
+        {
+            "$limit":20
+        },
+        {
+            "$project": {
+                "_id":0,
+                "City":"$_id",
+                "Total_Sales":1,
+                "Orders":1,
+                "Avg_Order":1    
+
+
+            }
+        }
+    ]
+
+    result =orders.aggregate(pipeline)
+    return list(result)
+    
+    # result = list(orders.aggregate(pipeline))
+
+    # table = []
+    # header = "City | Total Sales | Orders | Avg Order"
+    # table.append(header)
+
+    # for r in result:
+    #     row = f"{r['City']} | ${r['Total_Sales']:.2f} | {r['Orders']} | ${r['Avg_Order']:.2f}"
+    #     table.append(row)
+
+    # return {"table": table}
+ 
